f4c91638d696670383f293cab61edf8c4a619a7a
flschaap <141092891+flschaap@users.noreply.github.com>
2023-09-21 22:24:54 +1000

    Add files via upload

---
+++ im2350/a2/e4-music-to-relax-or-study-to/playerControls.js
@@ -0,0 +1,165 @@
+let audioElement = document.getElementById("audioElement");
+
+let playButton = document.getElementById("playButton");
+let stopButton = document.getElementById("stopButton");
+
+let overlay = document.getElementById("audioPlayOverlay")
+
+let progressBar = document.getElementById("progressBar");
+
+let heroImage = document.getElementById("heroImage");
+var display = 0;
+
+let focusButton = document.getElementById("foucsButton")
+
+// next we remove the controls attribute - we do this with JS rather than just not including it in the HTML tag as a fallback
+// this way if the JS doesn't load for whatever reason the player will have the default built in controls
+audioElement.removeAttribute("controls");
+// then if the default controls are removed we can show our custom controls - we want to do this via JS so that if the JS doesn't
+// load then they won't show
+document.getElementById("controlsWrapper").style.display = "flex";
+
+// then we listen for the loadedmetadata event to fire which means we'll be able to access exactly how long the piece of media is
+// i'm using an arrow function here that updates the progress element's max attribute with the duration of the media
+// this way when it comes to setting the progress bars value the number matches a percentage of the total duration
+audioElement.addEventListener('loadedmetadata', () => {
+  progressBar.setAttribute('max', audioElement.duration);
+});
+
+// some mobile devices won't fire a loadedmetadata event so we need a fallback to make sure the attribute is set in these cases - we 
+// can do this by also running a check whenever playback starts by using the playing event
+audioElement.addEventListener("playing", () => {
+  // we can then double check if the attribute has already been set - if not then set it here - ! inside of an if statement flips the 
+  // truth of what we're checking for - so (progressBar.getAttribute('max')) would check if there's a value and 
+  // (!progressBar.getAttribute('max')) checks if there is no value - ie false
+  if (!progressBar.getAttribute('max')){
+    progressBar.setAttribute('max', audioElement.duration);
+  }
+});
+
+/* LOADING */
+
+// here we're adding some feedback to indicate when the audio is loading - this is pretty similar to our last experiement in that we're 
+// applying an animation via a class. the real difference here is when that class gets added - by listening for the waiting event which 
+// fires when the media is waiting to load we can add the animation to the timeline via the .classList.add() method. when we want to 
+// stop the animation we listen for the canplay event which fires when the media player has buffered enough data to be able to playback the 
+// media then we use the .classList.remove() method - if we instead wanted to wait until it has actually loaded the whole file we could 
+// use the canplaythrough event
+audioElement.addEventListener("waiting", () => {
+  progressBar.classList.add("timeline-loading");
+});
+audioElement.addEventListener("canplay", () => {
+  progressBar.classList.remove("timeline-loading");
+});
+
+/* MEDIA FINSIHED */
+
+// when the media finishes we want to make sure that play icon switches back over from pause to indicate that the user can restart playback
+audioElement.addEventListener("ended", () => {
+  playButton.style.backgroundImage = "url('./icons/play.svg')";
+});
+
+/* PLAY/PAUSE */
+
+// we can use the .play() and .pause() methods on our media element to play and pause playback - because I want this to be triggered by 
+// two different events (see below) i'm going to write it as a seperate function 
+// by combining play and pause into the same function i'm able to make sure it does what i want - if the media is already playing i only 
+// ever want use .pause() (as pausing an already paused audio doesn't really make sense) 
+// the same goes if the media is paused or stopped i only want use .play()
+function playPause(){
+  if (audioElement.paused || audioElement.ended) {
+    audioElement.play();
+    playButton.style.backgroundImage = "url('./icons/pause.svg')";
+    overlay.style.display = "none";
+  } else {
+    audioElement.pause();
+    playButton.style.backgroundImage = "url('./icons/play.svg')";
+    overlay.style.display = "block";
+  }
+}
+
+// now we have our function we need to attach it to two seperate events, the first is probably obvious - clicking on the play button
+playButton.addEventListener('click', playPause);
+
+// the second event we want is clicking on the hero image, a feature popularised by youtube that is now ubiquitous in online media players
+heroImage.addEventListener('click', playPause);
+
+// Because the context of the media player is for it to be used for studying and/or relaxing, an image can be distracting. Therefor, 
+// a focus mode button has been added. The user has the ability to change to focus mode which will hide the image and will only display the controls
+function switchMode (){
+  if (display == 1){
+    heroImage.style.display = 'block';
+    display = 0;
+  } else {
+    heroImage.style.display = 'none';
+    overlay.style.display = 'none';
+    display = 1;
+  }
+}
+
+
+/* TIMELINE */
+
+// there's two different things we want to do with our timeline - update the progress bar to display how much has already played and let the user 
+// click the progress bar to scrub the audio to a specific place in the audio
+// to update the progress bar we need to listen for the timeupdate event which is fired everytime the current audio time is updated - when the audio 
+// is playing this repeatedly fires at a constant rate
+audioElement.addEventListener('timeupdate', () => {
+  // this statement is simple - we update the progress bar's value attribute with the currentTime property of the audio, because timeupdate runs everytime
+  // currentTime is changed it'll update both as the audio plays and if we were to skip or stop the audio
+  progressBar.value = audioElement.currentTime;
+});
+
+// the simplest version of scrubbing would be to update the audio's currentTime when the user clicks the timeline - however due to the interaction pattern 
+// established by youtube we should also account for a slightly different expression of user agency. the code below will work with a simple click on the 
+// timeline but will also allow for a user to drag their mouse on the timeline to continuously update currentTime and only end scrubbing when they release the 
+// mouse button. implementing this will take some more complex use of event listeners but i'll do my best to explain the design and technical implementation
+
+// first thing we want to do is write a function that will take the current position of the the mouse in relation to the timeline and use it to change the 
+// currentTime property of the audio element. each time this runs we'll need to know the position of the mouse so which we'll do using the event passed to it 
+// by the eventlistener - to access this we need to set it as a parameter, i've used the name e but it can be called whatever you like
+function scrubToTime(e){
+  // this statement has a lot going on so let's step through each part:
+  // the first thing we want to work out is the distance between the left side of the progress bar and the mouses current position - if we were just building 
+  // an interaction to work when the mouse is over the bar we could take this from the event, however as we want this to also work when we've held the mouse 
+  // down and moved it somewhere else on the page we need to work this out manually
+  // e.clientX is the cursors current distance from the left edge of the page
+  // we then want to minus (progressBar.getBoundingClientRect().left + window.scrollX) from this distance to account for any gap between the left edge of the 
+  // page and the start of the progress bar
+  // audioElement.currentTime is the current position in the media file - we are setting it here to change the playback time
+  // we then need to find a normalised 0-1 value based on how far along the bar the cursor is - the idea is that if i click the left most side it should return 0
+  // and if i click the right most side it should return 1 - we get this value by dividing x by the total width of the progressBar
+  // the value is then fed into our clampZeroOne() function - this is accounting for if our mouse is further left or further right than the ends of the progress bar
+  // it works by essentially making the value always equal 1 if it is over 1 or always making it 0 if under 0 - this is commonly called a clamp, we're only allowing
+  // a value to be in a certain range
+  // finally we're using this clamped value to multiply with total duration of our audio thus working out where we should scrub to
+  let x = e.clientX - (progressBar.getBoundingClientRect().left + window.scrollX);
+  audioElement.currentTime = clampZeroOne(x / progressBar.offsetWidth) * audioElement.duration;
+}
+
+// the click event fires only if the user presses the mouse down and then releases it on the same element. we can allow for a wider range of interactions by
+// further breaking this down this into its discrete parts and listening to both the mousedown and mouseup events seperately
+
+progressBar.addEventListener('mousedown', scrubToTime);
+progressBar.addEventListener('mousedown', (e) => {
+  // the behaviour here is to listen to the mousemove event (fired when the user moves their mouse) when the click is held down but then to stop listening to that 
+  // event when the mouse click is released
+  window.addEventListener('mousemove', scrubToTime);
+  window.addEventListener('mouseup', () => {
+    window.removeEventListener('mousemove', scrubToTime);
+  });
+});
+
+
+
+
+/* HELPER FUNCTIONS */
+
+function clampZeroOne(input){
+  return Math.min(Math.max(input, 0), 1);
+}
+
+function logEvent(e){
+  console.log(e);
+}
+
