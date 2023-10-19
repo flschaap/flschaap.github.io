716dad9253bf3e312a1518de4b3d4104a6c6c1a1
flschaap <141092891+flschaap@users.noreply.github.com>
2023-10-20 01:20:57 +1100

    Add files via upload

---
+++ im2350/a3/script.js
@@ -0,0 +1,35 @@
+const flower = document.querySelector('.flowerWrapper')
+const dropArea = document.querySelector('.dropArea')
+const infoDisplay = document.querySelector('#flowerWrapper')
+const soil = document.querySelector('.soilDropBack')
+
+flower.addEventListener('dragging', dragging)
+flower.addEventListener('dragstart', dragStart)
+
+dropArea.addEventListener('dragover',dragOver)
+dropArea.addEventListener('drop',dragDrop)
+
+soil.addEventListener('dragover',dragOver)
+soil.addEventListener('drop',dragDrop)
+
+let beingDragged
+flower.style.width = "25%";
+flower.style.height = "25%";
+
+function dragging(e) {
+    infoDisplay.textContent = 'You have picked a ' + beingDragged.id
+}
+
+function dragStart(e) {
+    beingDragged = e.target
+    dropArea.classList.add('highlight')
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.target.append(beingDragged)
+    dropArea.classList.remove('highlight')
+}
